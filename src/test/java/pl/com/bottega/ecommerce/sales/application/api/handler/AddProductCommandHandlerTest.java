e88571d4b176f185d2fb8989591737cfa2f89e4b
Kamil Żarski <209526@edu.p.lodz.pl>
2019-05-12 18:38:14 +0200

    refactor mocks, and init method

--- src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
+++ src/test/java/pl/com/bottega/ecommerce/sales/application/api/handler/AddProductCommandHandlerTest.java
@@ -28,51 +28,29 @@ import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AddProductCommandHandlerTest {
-
+    
     @Mock
     private ReservationRepository reservationRepository;
-
     @Mock
     private ProductRepository productRepository;
-
     @Mock
     private SuggestionService suggestionService;
-
     @Mock
     private ClientRepository clientRepository;
 
-    @Mock
+    private AddProductCommandHandler addProductCommandHandler;
+    private AddProductCommand addProductCommand;
+    private Client client;
     private SystemContext systemContext;
 
-    @Mock
-    private Reservation reservation;
-
-    @Mock
-    private Product product;
-
-    @Mock
-    private AddProductCommand addProductCommand;
-
-    @Mock
-    private Client client;
-
-    @Mock
-    private SystemUser systemUser;
-
-    private AddProductCommandHandler addProductCommandHandler;
-
-    private ArgumentCaptor<Product> products;
-
     @Before
-    public void setUp() throws Exception {
-        when(reservationRepository.load(any(Id.class))).thenReturn(reservation);
-        when(productRepository.load(any(Id.class))).thenReturn(product);
-        when(suggestionService.suggestEquivalent(any(), any())).thenReturn(product);
-        when(clientRepository.load(any())).thenReturn(client);
-        when(systemContext.getSystemUser()).thenReturn(systemUser);
-        // when(systemContext.getSystemUser().getClientId()).thenReturn(any(Id.class));
-        when(product.isAvailable()).thenReturn(true);
-        products = ArgumentCaptor.forClass(Product.class);
+    public void setUp() {
+        addProductCommand = new AddProductCommand(new Id("1"), new Id("1"), 100);
+        systemContext = new SystemContext();
+        client = new Client();
+        addProductCommandHandler =
+                new AddProductCommandHandler(reservationRepository, productRepository, suggestionService, clientRepository, systemContext);
+
     }
 
     @Test
